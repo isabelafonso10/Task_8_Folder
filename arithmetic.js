651e8894f5eef7b425f2930b2cf8f50124afa4d9
isabelafonso10 <isabelafonso_528@hotmail.com>
2024-12-11 22:17:27 +0000

    Add files via upload

---
+++ arithmetic.js
@@ -0,0 +1,33 @@
+﻿function findSum(numArray) {
+    let add =0;
+    for (let a = 0; a < numArray.length; a++) {
+        let addindex = numArray[a];
+        add += addindex;
+
+    }
+    return add;
+}
+
+ function subtractNumbers(num);
+ let a = 20
+ let b = 40
+ return c
+
+ console.log(c);
+
+ function multiplyNumbers( x, y); {
+    let x = 6
+    let y = 9
+    let z = multiplyNumbers(6,9);
+    return x*y
+ }
+
+function divideNumbers( numerator, denominator); {
+    if (denominator === 0 || isNaN(denominator)) {
+        return null
+    }
+    else {
+        return numerator / denominator
+    }
+}
+

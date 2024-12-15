506934a735aaf618ecd2bdbd48fed3fb0697a1a8
isabelafonso10 <isabelafonso_528@hotmail.com>
2024-12-15 11:50:44 +0000

    Add files via upload

---
+++ palindrome.js
@@ -0,0 +1,11 @@
+﻿// User input
+
+ let word = prompt("Please enter a word");
+ function isPalindrome(word) { let l = word.length; 
+for (let i = 0; i < l / 2; i++) { 
+    if (word.charAt(i) !== word.charAt(l - 1 - i)) { return false; } 
+} return true; 
+} if (isPalindrome("")) { 
+    document.write("The word is a palindrome"); 
+} else { document.write("The word is NOT a palindrome");
+}

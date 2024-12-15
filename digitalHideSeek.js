3b12326f176d8365297017e8d06562a10e32c013
isabelafonso10 <isabelafonso_528@hotmail.com>
2024-12-15 11:51:09 +0000

    Add files via upload

---
+++ digitalHideSeek.js
@@ -0,0 +1,10 @@
+// DEfine a function hide that takes in an argument, representing a hiden location. This function should store the location in a local variable hidelocation.
+function hide(a) {
+    function seek() {
+        const hideLocation = a;
+        return hideLocation;
+
+    }
+}
+const playNow = hide("Behind the door");
+console.log(playNow());
